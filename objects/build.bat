78b59162dec47a55f8671bfe03f206742847683f
Philip Thong <philip.thong@ni.com>
2019-09-11 14:20:08 -0500

    <hack> now building ALL_BUILD target

--- objects/build.bat
+++ objects/build.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;"c:\program files\cmake\bin"
 cmake -Dall=OFF -Dmathmore=OFF -Dbuiltin_zlib=ON -Dgfal=OFF -Dbuiltin_freetype=ON -Dbuiltin_ftgl=ON -Droofit=OFF -Dfftw3=OFF -DCMAKE_INSTALL_PREFIX=".\export" -G"Visual Studio 16 2019" -A Win32 ..\
-cmake --build . --config RelWithDebInfo
+cmake --build . --config RelWithDebInfo --target ALL_BUILD
