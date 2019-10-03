7ca5e7583e340538bb45b892fa7f118d3c1e586d
Philip Thong <philip.thong@ni.com>
2019-10-03 10:58:59 -0500

    shelve

--- objects/build.bat
+++ objects/build.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;"c:\program files\cmake\bin"
-cmake -Dall=OFF -Dmathmore=OFF -Dbuiltin_zlib=ON -Dbuiltin_lzma=ON -Dgfal=OFF -Dbuiltin_freetype=ON -Dbuiltin_ftgl=ON -Droofit=OFF -Dfftw3=OFF -DCMAKE_INSTALL_PREFIX=".\export" -G"Visual Studio 16 2019" -A x64 -Thost=x64 ..\
-cmake --build . --config RelWithDebInfo --target ALL_BUILD
+cmake -Dall=OFF -Dmathmore=OFF -Dbuiltin_zlib=ON -Dbuiltin_lzma=ON -Dgfal=OFF -Dbuiltin_freetype=ON -Dbuiltin_ftgl=ON -Droofit=OFF -Dfftw3=OFF -DCMAKE_INSTALL_PREFIX="../export" -G"Visual Studio 16 2019" -A x64 -Thost=x64 ..\
+rem cmake --build . --config RelWithDebInfo --target ALL_BUILD
