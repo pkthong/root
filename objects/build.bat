a417f64342cfd47d0b201420982268230641a30c
Philip Thong <philip.thong@ni.com>
2019-11-23 09:42:42 -0600

    <hack> Win64 build script

--- objects/build.bat
+++ objects/build.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;"c:\program files\cmake\bin"
-cmake -Dall=OFF -Dmathmore=OFF -Dbuiltin_zlib=ON -Dbuiltin_lzma=ON -Dgfal=OFF -Dbuiltin_freetype=ON -Dbuiltin_ftgl=ON -Droofit=OFF -Dfftw3=OFF -DCMAKE_INSTALL_PREFIX="../export" -G"Visual Studio 16 2019" -A Win32 ..\
+cmake -Dall=OFF -Dmathmore=OFF -Dbuiltin_zlib=ON -Dbuiltin_lzma=ON -Dgfal=OFF -Dbuiltin_freetype=ON -Dbuiltin_ftgl=ON -Droofit=OFF -Dfftw3=OFF -DCMAKE_INSTALL_PREFIX="../export" -Dtesting=ON -G"Visual Studio 16 2019" -A x64 -Thost=x64 ..\
 rem cmake --build . --config RelWithDebInfo --target ALL_BUILD
